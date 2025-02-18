f4246507576454c581a2e150f2f9cd23e664fab3
Amondt <mondt.alexandre@gmail.com>
2025-02-18 21:34:07 +0100

    add project structure

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import *
+
+def main():
+    my_text_node = TextNode("This is a text node", InlineText.BOLD, "https://www.boot.dev")
+    print(my_text_node)
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,26 @@
+from enum import Enum
+from typing import Self
+
+class InlineText(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode:
+    def __init__(self, text: str, text_type: InlineText, url: str | None = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, text_node: Self) -> bool:
+        return (
+            self.text == text_node.text
+            and self.text_type == text_node.text_type
+            and self.url == text_node.url
+        )
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.text}, {self.text_type.value}, {self.url})"
